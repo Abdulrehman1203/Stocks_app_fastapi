f42645c2bec9d1f61a304ded69c43a80c3894e26
Umer <umershahzad2003kpr@gmail.com>
2024-10-09 21:17:36 +0500

    Added celery and redis

--- PycharmProjects/fastApi_stocks_project/config/config.py
+++ PycharmProjects/fastApi_stocks_project/config/config.py
@@ -1,5 +1,29 @@
+from celery import Celery
+
+
 class Settings:
     SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres:5571@localhost:5433/new_db"
 
 
 settings = Settings()
+
+CELERY_BROKER_URL = "redis://localhost:6379/0"
+CELERY_RESULT_BACKEND = "redis://localhost:6379/0"
+
+celery = Celery("tasks",
+                broker=CELERY_BROKER_URL,
+                backend=CELERY_RESULT_BACKEND,
+                include=["tasks"])
+
+
+celery.conf.update(
+    enable_utc=True,
+    timezone='UTC',
+    beat_schedule={
+        'fetch_stocks_every_minute': {
+            'task': 'tasks.fetch_all_stocks',
+            'schedule': 10.0,  # Run every 60 seconds
+        },
+    }
+)
+
